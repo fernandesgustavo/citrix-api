0ce04be5ebcf6f052331cbce6b061d7d69e9ebbe
Gustavo Fernandes <gustavo.fernandes@solor.com.br>
2019-08-29 11:05:19 -0300

    Consultas para session.

--- session.py
+++ session.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from datetime import timedelta
 from configparser import ConfigParser
 
-class Citrix:
+class Session:
     
     def __init__(self, company, customer, client_id, client_secret, token, token_created):
 
@@ -44,24 +44,42 @@ class Citrix:
             config.set(self.__company, 'token_created', str(self.__date_now))
             with open('./config.cfg', 'w') as f:
                 config.write(f)
+    
+    def sessions_disconnected(self):
 
-    def getMetric(self, metric_name):
-        
-        url_final = '{0}{1}'.format(self.__url_metrics, metric_name)
-        params = {'$top': 2}
+        url_final = '{}{}'.format(self.__url_metrics, 'Sessions')
+        params = {'$apply': 'groupby((ConnectionState),aggregate(SessionKey with countdistinct as NumberOfSessions))',
+                    '$filter': 'ConnectionState eq 2'}
         response = requests.get(url_final, headers=self.__headers, params=params)
-        print(response.text)
+        return (response.json()['value'][0]['NumberOfSessions'])
+
+    def sessions_terminated(self):
+
+        url_final = '{}{}'.format(self.__url_metrics, 'Sessions')
+        params = {'$apply': 'groupby((ConnectionState),aggregate(SessionKey with countdistinct as NumberOfSessions))',
+                    '$filter': 'ConnectionState eq 3'}
+        response = requests.get(url_final, headers=self.__headers, params=params)
+        return (response.json()['value'][0]['NumberOfSessions'])
+
+    def sessions_active(self):
+
+        url_final = '{}{}'.format(self.__url_metrics, 'Sessions')
+        params = {'$apply': 'groupby((ConnectionState),aggregate(SessionKey with countdistinct as NumberOfSessions))',
+                  '$filter': 'ConnectionState eq 5'}
+        response = requests.get(url_final, headers=self.__headers, params=params)
+        return (response.json()['value'][0]['NumberOfSessions'])
 
 if __name__ == '__main__':
 
+    # objeto ConfigParser
+    file = 'config.cfg'
+    config = ConfigParser()
+    config.read(file)
+
     # SOLOR
     company = sys.argv[1]
-    # Sessions
+    # active
     metric = sys.argv[2]
-
-    # objeto ConfigParser
-    config = ConfigParser()
-    config.read('config.cfg')
 
     # obtem as métricas através do arquivo de configuração
     customer = config.get(company, 'customer')
@@ -70,7 +88,13 @@ if __name__ == '__main__':
     token = config.get(company, 'token')
     token_created = config.get(company, 'token_created')
 
-    # cria um objeto do tipo Citrix
-    citrix = Citrix(company, customer, client_id, client_secret, token, token_created)
-    citrix.generateToken()
-    citrix.getMetric(metric)
+    # cria um objeto do tipo Session
+    session = Session(company, customer, client_id, client_secret, token, token_created)
+    session.generateToken()
+
+    if metric == 'disconnected':
+        print('{}'.format(session.sessions_disconnected()))
+    elif metric == 'terminated':
+        print('{}'.format(session.sessions_terminated()))
+    elif metric == 'active':
+        print('{}'.format(session.sessions_active()))
